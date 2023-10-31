b8704e1463b12ae26bc8958b3051c00fd6f0a8f2
ednganji <ednganji01@monccnb.ca>
2023-10-31 12:49:17 -0300

    fin

--- src/PahoTest001Producer.java
+++ src/PahoTest001Producer.java
@@ -7,6 +7,10 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.util.UUID;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+import com.google.gson.Gson;
+
 public class PahoTest001Producer implements MqttCallback {
 
 MqttClient client;
@@ -14,13 +18,73 @@ MqttClient client;
 public PahoTest001Producer() {
 }
 
+public static Double rand() {
+        Random random = new Random();
+        
+        // Generate a random double between 26 and 28
+        double randomValue = 26 + random.nextDouble() * 2;
+        
+        // Round the value to two decimal places
+        double roundedValue = Math.round(randomValue * 100.0) / 100.0;
+        
+        // Format the rounded value to have exactly two decimal places
+        DecimalFormat df = new DecimalFormat("0.00");
+        String formattedValue = df.format(roundedValue);
+        
+        System.out.println("Random decimal value: " + formattedValue);
+        return randomValue;
+    }
+
+
+        public static Integer randhum() {
+            Random random = new Random();
+            
+            int minValue = 20;
+            int maxValue = 30;
+            
+            // Generate a random integer between 20 and 30 (inclusive)
+            int randomValue = random.nextInt((maxValue - minValue) + 1) + minValue;
+            
+            return randomValue;
+        }
+    
+
+   
+  public  static String CreateJson() {
+            // Créez une instance de la classe Person avec des variables Java
+            Double Temperature = rand();
+            int Humidite = randhum();
+            Capteur capteur = new Capteur(Temperature, Humidite);
+    
+            // Utilisez Gson pour convertir l'objet en une chaîne JSON
+            Gson gson = new Gson();
+            String jsonString = gson.toJson(capteur);
+    
+            System.out.println("JSON string: " + jsonString);
+            return jsonString;
+      }
+    
+
 public static void main(String[] args) {
-    new PahoTest001Producer().doDemo("Bonjour");
+    while (true) {
+        new PahoTest001Producer().doDemo(CreateJson());
+
+        try {
+            // Mettez la boucle en pause pendant 1000 millisecondes (1 seconde)
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+     
+   // new PahoTest001Producer().doDemo("{\"Temperature\":\"50\",\"Humidite\":\"50\"}");
+    
 }
 
 public void doDemo(String valeur) {
     try {
         String uri = "tcp://test.mosquitto.org:1883";
+        //String uri = "tcp://192.168.1.216:1883";
         String clientID = UUID.randomUUID().toString();
         MemoryPersistence persistence = new MemoryPersistence();
         System.out.println("*** uri = "+uri);
@@ -33,7 +97,7 @@ public void doDemo(String valeur) {
         MqttMessage message = new MqttMessage();
         message.setPayload(valeur.getBytes());
         System.out.println("*** msgId = "+message.getId());
-        client.publish("foo", message);
+        client.publish("foo2", message);
 
         client.disconnect();
     } catch (MqttException e) {
@@ -57,3 +121,6 @@ public void deliveryComplete(IMqttDeliveryToken token) {
 }
 
 }
+
+
+
